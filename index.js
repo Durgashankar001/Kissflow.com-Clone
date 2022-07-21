c17029487a998d848aae28ab1dd4dbfb88ebadb8
abajaj655 <abajaj655@gmail.com>
2022-07-21 20:45:49 +0530

    day-3 task done

--- index.js
+++ index.js
@@ -27,3 +27,20 @@ button.addEventListener("click", function(event){
 //     document.getElementById("container").style.filter = "blur(0px)";
 
 // })
+
+let tabs = document.querySelector(".tabs");
+let tabHeader = tabs.querySelector(".tab-header");
+let tabBody = tabs.querySelector(".tab-body");
+let tabIndicator = tabs.querySelector(".tab-indicator");
+let tabHeaderNodes = tabs.querySelectorAll(".tab-header > div");
+let tabBodyNodes = tabs.querySelectorAll(".tab-body > div");
+
+for(let i=0; i<tabHeaderNodes.length; i++){
+  tabHeaderNodes[i].addEventListener("click",function(){
+    tabHeader.querySelector(".active").classList.remove("active");
+    tabHeaderNodes[i].classList.add("active");
+    tabBody.querySelector(".active").classList.remove("active");
+    tabBodyNodes[i].classList.add("active");
+    // tabIndicator.style.left = `calc(calc(calc(14% - 5px) * ${i}) + 30px)`
+  })
+}
